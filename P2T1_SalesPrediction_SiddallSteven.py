27ab2f2504252b4ebecc40e3f987477d63d056b5
siddalls3135 <39928770+siddalls3135@users.noreply.github.com>
2018-06-09 10:49:16 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_SiddallSteven.py
@@ -0,0 +1,14 @@
+# CTI-110 
+# P2T1 - Sales Prediction 
+# Steven Siddall
+# 03 June 2018
+# Get the projected total sales
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 20 percent of total sales
+profit = total_sales * 0.23
+
+# Display the profit.
+print('the profit is $', format(profit, ',.2f'))
+
+
